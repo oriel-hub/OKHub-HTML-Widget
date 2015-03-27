c0b69785eebf0fcca8d9214f564dc0ea90fee173
Knowledge Services API <api@ids.ac.uk>
2015-03-27 11:14:51 +0000

    Added explanatory text and style changes

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>OKHub Web Widget Customisation</title>
+		<title>OKhub Web Widget Customisation</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 		<?php include('/var/www/includes/nav.shtml'); ?>
 		<link href="http://www.okhub.org/static/globalnav.css" rel="stylesheet" type="text/css">
+		<link href="http://data.okhub.org/sites/all/themes/skeletontheme/css/style.css" rel="stylesheet" type="text/css">
 		<link href="okhub.css" rel="stylesheet" type="text/css">
-		<link href="widgetadmin.css" rel="stylesheet" type="text/css">
+		<link href="style.css" rel="stylesheet" type="text/css">
 		<script type="text/javascript">
 			var widgetadminurl = '<?php 
 			$widget_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
@@ -16,34 +19,47 @@
 	</head>
 	<body>
 
-		<div id="wrap">
+	<div id="wrap">
+		<div id="okhub-widget">
+
 			<div id="dynamic-demo-hub-widget">
-				<script src="<?php echo $widget_url; ?>okhub_widget.js?type=search&_token_guid=5c96d95b-c729-4624-b1c2-14c6b98dc9ce" type="text/javascript"></script>
+				<script src="<?php echo $widget_url; ?>okhub_widget.js?type=search&_token_guid=50ae9726-efa9-4b40-a623-85823df5aac2" type="text/javascript"></script>
 				<div id="open-knowledge-hub-widget">Sample Widget</div>
 			</div>
-			<div id="main_container">
+			<div id="open-knowledge-hub-widget-main-container">
 	
-				<h2>OkHub Javascript Web Widget</h2>
+				<h2>OKHub Javascript Web Widget</h2>
+								
+				<div class="text_container">
+					<p><strong>Want to easily give your users the chance to search through 1000's of freely accessible research documents on poverty reduction in the developing world, from the OKHub content providers?</strong></p>
+					<p>The OKHub Javascript Web Widget allows you to create content relevant to your users and embed it in your website with a few simple lines of code. The widget then automatically displays the latest development research from a wide variety of trusted sources based on your preferences, like the widget to the right.</p>
+					<p><strong>Why not create a widget for your site today?</strong></p>
+				</div>
 				
 				<div class="step" id="step1">
 					<p><span class="steptext">Step 1.</span>  Customise the widget (optional)</p>
 					<div class="form-item"><input type="text"
 						name="q" placeholder="Search Parameter e.g. Water"
-						style="width: 100%; height: 30px;" /></div>  
+
+						style="" /></div>  
 					<div class="form-item"><input type="text"
 						name="country" placeholder="Country/Region e.g. India"
-						style="width: 100%; height: 30px;" /></div> 
-					<div class="form-item"><input type="text"
-						name="theme" placeholder="Theme e.g. Education" style="width: 100%; height: 30px;" /></div> 
+
+						style="" /></div> 
+					<div class="form-item" style="display:none;"><input type="text"
+						name="theme" placeholder="Theme e.g. Education" style="" /></div> 
 					<div class="form-item"><input type="text" name="widget_title"
 						placeholder="Title for the Widget e.g. Development Resources"
-						style="width: 100%; height: 30px;" /></div> 
+
+						style="" /></div> 
 					<div class="form-item"><input type="text" name="text_color"
 						placeholder="Widget Text Colour e.g. red or #ff0000"
-						style="width: 100%; height: 30px;" /></div> 
+
+						style="" /></div> 
 					<div class="form-item"><input type="text" name="background_color"
 						placeholder="Widget Background Colour e.g. red or #ff0000"
-						style="width: 100%; height: 30px;" /></div> 
+
+						style="" /></div> 
 					<div class="form-item form-submit"><input
 						type="submit" name="customisesubmit" value="Update" /></div>
 					<p class="note">Note: You can also use Boolean Logic for filtering results. For Search Parameter e.g. "Gender AND Climate". For Country/Region e.g. "India OR Namibia"</p>
@@ -56,7 +72,8 @@
 				</p>
 				<div class="form-item"><input type="text" name="apikey"
 						placeholder="Enter you API Key (this is required)"
-						style="width: 100%; height: 30px;" /></div> 
+
+						style="" /></div> 
 				<div class="form-item form-submit"><input
 						type="submit" name="apikeysubmit" value="Submit API key" /></div>
 				</div>
@@ -74,7 +91,7 @@
 
 			</div>
 		</div>
-		<!--wrap-->
+	</div><!--wrap-->
 
 	</body>
 </html>
