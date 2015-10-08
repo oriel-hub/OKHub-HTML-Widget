b36fb139cc86fd8172f512153f63330576f29cf2
Joe McPherson <j.m.p.mcpherson@gmail.com>
2015-10-08 10:51:24 +0100

    functionality/Styling for selected tabs. rationalised tab code

--- okhub-widget.js
+++ okhub-widget.js
@@ -11,7 +11,7 @@
 						"http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
 		if (script_tag.readyState) {
 			script_tag.onreadystatechange = function() { // Support for old
-															// versions of IE
+				// versions of IE
 				if (this.readyState == 'complete'
 						|| this.readyState == 'loaded') {
 					scriptLoadHandler();
@@ -38,7 +38,7 @@
 	 * get script tags and returns reference to our okhub-widget.js will be used
 	 * to get js parameters
 	 */
-	
+
 	function getScriptUrl() {
 		var scriptUrl = null;
 		jQuery('script.okhub-widget-script').each(function() {
@@ -46,7 +46,7 @@
 		});
 		return scriptUrl;
 	}
-	
+
 	/* fetch parameters */
 	function getQueryParameters(query) {
 		var args1 = query.split('?');/* split uri */
@@ -66,7 +66,7 @@
 		}
 		return params;
 	}
-	
+
 	function getOtherParams(params) {
 		var p = [];
 		if (params.q !== undefined) {
@@ -80,7 +80,7 @@
 		}
 		return p.join("&");
 	}
-	
+
 	function hub_navigate(params) {
 		jQuery('#open-knowledge-hub-widget-content').html(
 				"<div class='okhub-widget-message'>Searching....</div>");
@@ -95,13 +95,14 @@
 				.getJSON(
 						jsonp_url,
 						function(data) {
-							if(!data.metadata.total_results){
+							if (!data.metadata.total_results) {
 								jQuery('#open-knowledge-hub-widget-content')
-								.html(
-										"<h4>No results found for search criteria...</h4>");
+										.html(
+												"<h4>No results found for search criteria...</h4>");
 							} else {
 								var num_pages = 50;
-								var pages = num_pages + Number(params.start_offset);
+								var pages = num_pages
+										+ Number(params.start_offset);
 								var counter = 0;
 								if (typeof data.metadata.prev_page != "undefined") {
 									footer = footer
@@ -112,19 +113,22 @@
 									for ( var i = Number(params.start_offset) + 10; i < pages; i = i + 10) {
 										counter++;
 										if (counter < 5) {
-											if((counter * 10) <= data.metadata.total_results - params.start_offset){
+											if ((counter * 10) <= data.metadata.total_results
+													- params.start_offset) {
 												var page_id = "pageid_" + i;
 												if (counter == 1) {
 													footer = footer
 															+ "<button id="
 															+ page_id
 															+ " class='selected_button'>"
-															+ i / 10 + "</button>";
-		
-												} else {
-													footer = footer + "<button id="
-															+ page_id + ">" + i / 10
+															+ i / 10
 															+ "</button>";
+
+												} else {
+													footer = footer
+															+ "<button id="
+															+ page_id + ">" + i
+															/ 10 + "</button>";
 												}
 											}
 										}
@@ -137,27 +141,27 @@
 
 								var searchfeedbacktext = '';
 								if (params.q != undefined) {
-									searchfeedbacktext = 
-											" search results for "
+									searchfeedbacktext = " search results for "
 											+ params.q;
 								} else {
 									searchfeedbacktext = " search results";
-								}								
-								output = output + "<div class='okhub-widget-message'><h5>"
+								}
+								output = output
+										+ "<div class='okhub-widget-message'><h5>"
 										+ data.metadata.total_results
-										+ searchfeedbacktext
-										+ "</h5></div>";
+										+ searchfeedbacktext + "</h5></div>";
 								if (typeof data.results != "undefined") {
 									okhub_results(output, data.results, params)
 								} else {
-									//alert("Failed to connect to OKHub, please try again later...");
+									// alert("Failed to connect to OKHub, please
+									// try again later...");
 									jQuery('#open-knowledge-hub-widget-content')
 											.html(
 													"<h4>Unable to connect to OKHub, please try again later...</h4>");
 									return false;
 								}
-								jQuery('#open-knowledge-hub-widget-footer').html(
-										footer);
+								jQuery('#open-knowledge-hub-widget-footer')
+										.html(footer);
 								jQuery('button#okhub_next')
 										.click(
 												function(e) {
@@ -194,17 +198,17 @@
 							}
 						});
 	}
-	
+
 	function hub_search(param, token_guid) {
 		var params = {};
 		params._token_guid = token_guid;
 		params.q = param;
 		params.start_offset = 0;
 		jQuery('#open-knowledge-hub-widget-content').html(
-		"<div class='okhub-widget-message'>Searching ....</div>");
+				"<div class='okhub-widget-message'>Searching ....</div>");
 		hub_navigate(params);
 	}
-	
+
 	function okhub_clear_contents() {
 		jQuery('#okhub-overlay').hide();
 		jQuery('#okhub-modal').hide();
@@ -212,7 +216,7 @@
 		jQuery('#okhub-content').empty();
 		jQuery('#okhub-content-tabs').empty();
 	}
-	
+
 	function okhub_details(id, guid) {
 		var top, left;
 		var args = id.split('_');/* split parameter */
@@ -232,7 +236,8 @@
 							sourcesData = data.sources;
 							data = data.data;
 							if (typeof data.results == "undefined") {
-								//alert("Failed to connect to OKHub, please try again later...");
+								// alert("Failed to connect to OKHub, please try
+								// again later...");
 								okhub_clear_contents();
 								jQuery('#open-knowledge-hub-widget-content')
 										.html(
@@ -244,55 +249,50 @@
 										.each(
 												data.results,
 												function(index, value) {
-													for ( var i in value) {
-														title = value[i].title;
-														authors = value[i].authors;
-														url = value[i].url;
-														description = value[i].description;
-														year = value[i].publication_year;
-														publisher = value[i].publisher;
-														version = i;
-														object_id = value[i].object_id;
-														if(jQuery.isArray(url)){
-															url = url[0];
-														}
-														break;
-													}
+
 													var source_cnt = 0;
+													sources_tabs = '';
 													for ( var i in value) {
 														source_cnt++;
-														sources_tabs = sources_tabs
-																+ "<li id=oksource_"
+														sources_tabs +=
+																"<li id=oksource_"
 																+ i
-																+ "><a href='#'>"
-																+ i
-																		.toUpperCase()
+																+ " class='oksource_tab";
+														if(source_cnt == 1){
+															sources_tabs += ' selected';
+														}
+														sources_tabs += "'>"
+																+ "<a href='#'>"
+																+ sourcesData[i]
 																+ "</a></li> ";
 													}
-													if (source_cnt > 1) {
-														sources_tabs = "<ul class='okhub_sources'>" + sources_tabs + "</ul>";
-														jQuery(
-																'#okhub-content-tabs')
-																.html(sources_tabs);
-														jQuery(
-																'#okhub-content-tabs')
-																.show();
-													} else {
-														jQuery(
-																'#okhub-content-tabs')
-																.hide();
-													}
+													sources_tabs = "<ul class='okhub_sources'>"
+															+ sources_tabs
+															+ "</ul>";
+													jQuery(
+															'#okhub-content-tabs')
+															.html(sources_tabs);
+													jQuery(
+															'#okhub-content-tabs')
+															.show();
 													for ( var i in value) {
 														jQuery("#oksource_" + i)
 																.click(
 																		function(
 																				e) {
+																			jQuery('.oksource_tab').removeClass('selected');
+																			
 																			var ii = e.currentTarget.id
 																					.split("_");
 																			var k = ii[1];
+																			jQuery('#oksource_' + k).addClass('selected');
 																			title = value[k].title;
 																			authors = value[k].authors;
 																			url = value[k].url;
+																			if (jQuery
+																					.isArray(url)) {
+																				url = url[0];
+																			}
 																			description = value[k].description;
 																			year = value[k].publication_year;
 																			publisher = value[k].publisher;
@@ -343,45 +343,17 @@
 
 																		});
 													}
-													headertitle = "<h3><a href='"
-															+ url
-															+ "' target=_new>"
-															+ title
-															+ "</a></h3>";
-													if (authors) {
-														headertitle = headertitle
-																+ authors;
+													for ( var i in value) {
+														/* load first tab */
+														$("#oksource_" + i)
+																.trigger(
+																		"click");
+														break;
 													}
-													if (publisher) {
-														headertitle = headertitle
-																+ "<br/><i>"
-																+ publisher
-																+ "</i>";
-													}
-													if (year) {
-														headertitle = headertitle
-																+ " ("
-																+ year
-																+ ")";
-													}
-													if (version) {
-														headertitle = headertitle
-																+ "<br/>Source: "
-																+ sourcesData[version];
-													}
-													jQuery(
-															'#okhub-content-header')
-															.html(headertitle);
-													if (description) {
-														jQuery('#okhub-content')
-																.show();
-														jQuery('#okhub-content')
-																.html(
-																		"<p>"
-																				+ description
-																				+ "</p>");
-													} else {
-														jQuery('#okhub-content')
+
+													if (source_cnt < 2) {
+														jQuery(
+																'#okhub-content-tabs')
 																.hide();
 													}
 
@@ -403,7 +375,7 @@
 			okhub_clear_contents();
 		});
 	}
-	
+
 	function okhub_results(output, titles, params) {
 		output = output + "<ul  class='okhub_list'>";
 		jQuery.each(titles, function(index, value) {
@@ -439,7 +411,7 @@
 		});
 
 	}
-	
+
 	/** ******main function ******* */
 	function main() {
 		jQuery(document)
@@ -459,8 +431,7 @@
 									+ "<div id='okhub-content-tabs'></div>"
 									+ "<div id='okhub-content'></div>"
 									+ "<a href='#' id='okhub-close'>X</a>"
-									+ "</div></div>"
-									+ "</div>";
+									+ "</div></div>" + "</div>";
 							$('#open-knowledge-hub-widget').html(stru);
 							$('#okhub-overlay').hide();
 							$('#okhub-modal').hide();
